07548300f9b2c39e88de67564c841926108e9773
Fred <ricardofredt@hotmail.com>
2022-06-21 00:21:15 -0300

    convert function done

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ const url1 =
 	"https://contadearmazenamento42.blob.core.windows.net/container-faccat/book.xml"
 const url2 = "https://w3schools.com/xml/note.xml"
 
-const result1 = convertXmlToJson(url1).then((result) => {
-	console.log(result)
-})
+const result1 = convertXmlToJson(url1)
+	.then((result) => {
+		console.log(result)
+	})
+	.catch((err) => {
+		console.log(err)
+	})
 
-const result2 = convertXmlToJson(url2).then((result) => {
-	console.log(result)
-})
+const result2 = convertXmlToJson(url2)
+	.then((result) => {
+		console.log(result)
+	})
+	.catch((err) => {
+		console.log(err)
+	})
 
 // console.log(result1, result2)
